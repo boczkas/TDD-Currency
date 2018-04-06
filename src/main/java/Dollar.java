80e896c081adf91258b990889b435dc21f6a4553
boczkas <przemyslawjakubowski@gmail.com>
2018-04-06 10:48:24 +0200

    Chapter 2 example

---
+++ src/main/java/Dollar.java
@@ -0,0 +1,12 @@
+public class Dollar {
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier){
+        return new Dollar(amount * multiplier);
+    }
+
+}
